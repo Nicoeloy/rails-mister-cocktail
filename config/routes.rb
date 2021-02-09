e0f1d4675cd781d3a44c7ececdffd4df1804470b
Nicolas Eloy <colasrsca@gmail.com>
2021-02-09 18:19:55 +0100

    Doses controller -corr

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   root to: "cocktails#index"
 
-  resources :cocktails, only: [:index, :show, :new, :create]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
+  end
+  resources :doses, only: [:destroy]
 
 end
